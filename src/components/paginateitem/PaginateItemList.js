96d5fbf35fa63b797545235038781614b20233d0
minhnv194801 <91465533+minhnv194801@users.noreply.github.com>
2023-02-26 21:27:38 +0700

    up

--- src/components/paginateitem/PaginateItemList.js
+++ src/components/paginateitem/PaginateItemList.js
@@ -6,7 +6,7 @@ const PaginateItemList = (props) => {
     return (
         <Grid container spacing={2}>
             {props.items.map((item) => 
-                <Grid key={item.id} item xs={12} md={3}>
+                <Grid key={item.id} item xs={12} md={4}>
                     <PaginateItem item={item}/>
                 </Grid>
             )}
