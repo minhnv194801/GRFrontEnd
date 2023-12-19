8094f526744280557ad29369bb099220e69cb4a4
minhnv194801 <91465533+minhnv194801@users.noreply.github.com>
2023-12-19 16:09:27 +0700

    fix

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -19,8 +19,8 @@ function Home() {
   const recommendItemCount = 10
   const loadingItem = [{
     "id": "",
-    "cover": `${process.env.PUBLIC_URL}/circleload.webm`,
-    "image": `${process.env.PUBLIC_URL}/flippingbook.webm`,
+    "cover":'/circleload.webm',
+    "image": '/flippingbook.webm',
     "name": "Loading...",
     "href": "",
     "chapters": []
